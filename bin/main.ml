9b9a4798c05eb90d301a43380f831761ed3d7fe5
benmandrew <benmandrew@gmail.com>
2026-01-02 14:48:55 +0000

    feat: Commandline options for CLI
    Allow user to choose ASCII or SVG output.

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,16 @@
 open Oxqr
 open Base
 
-let run data ecl =
+type output_format = Svg | Ascii
+
+let run data ecl format =
   let qr = Encoding.generate_qr data ecl in
-  let qr_string = Qr.to_unicode_string qr in
-  Out_channel.output_string Out_channel.stdout qr_string
+  let output_string =
+    match format with
+    | Svg -> Svg.qr_to_svg qr
+    | Ascii -> Qr.to_unicode_string qr
+  in
+  Out_channel.output_string Out_channel.stdout output_string
 
 let string_of_ecl = function
   | Config.ECL.L -> "L"
@@ -23,6 +29,15 @@ let parse_ecl s =
         (Printf.sprintf "Error: ECL must be one of L, M, Q, H (got '%s')\n" s);
       Stdlib.exit 2
 
+let parse_format s =
+  match String.lowercase s with
+  | "svg" -> Svg
+  | "ascii" -> Ascii
+  | _ ->
+      Out_channel.output_string Out_channel.stderr
+        (Printf.sprintf "Error: FORMAT must be one of ascii, svg (got '%s')\n" s);
+      Stdlib.exit 2
+
 let print_help () =
   Out_channel.output_string Out_channel.stdout "Usage: oxqr [OPTIONS] DATA\n";
   Out_channel.output_string Out_channel.stdout
@@ -31,10 +46,12 @@ let print_help () =
   Out_channel.output_string Out_channel.stdout
     "  --ecl ECL       Error correction level (L|M|Q|H). Default: M.\n";
   Out_channel.output_string Out_channel.stdout
+    "  --format FORMAT Output format (ascii|svg). Default: ascii.\n";
+  Out_channel.output_string Out_channel.stdout
     "  -h, --help      Show this help message.\n"
 
-let rec parse_args argv idx data ecl =
-  if idx >= Array.length argv then (data, ecl)
+let rec parse_args argv idx data ecl format =
+  if idx >= Array.length argv then (data, ecl, format)
   else
     match argv.(idx) with
     | "-h" | "--help" ->
@@ -47,7 +64,15 @@ let rec parse_args argv idx data ecl =
           Stdlib.exit 2)
         else
           let new_ecl = parse_ecl argv.(idx + 1) in
-          parse_args argv (idx + 2) data new_ecl
+          parse_args argv (idx + 2) data new_ecl format
+    | "--format" ->
+        if idx + 1 >= Array.length argv then (
+          Out_channel.output_string Out_channel.stderr
+            "Error: --format requires an argument\n";
+          Stdlib.exit 2)
+        else
+          let new_format = parse_format argv.(idx + 1) in
+          parse_args argv (idx + 2) data ecl new_format
     | arg when String.is_prefix arg ~prefix:"-" ->
         Out_channel.output_string Out_channel.stderr
           (Printf.sprintf "Error: Unknown option '%s'\n" arg);
@@ -57,12 +82,14 @@ let rec parse_args argv idx data ecl =
           Out_channel.output_string Out_channel.stderr
             "Error: Too many positional arguments\n";
           Stdlib.exit 2)
-        else parse_args argv (idx + 1) (Some arg) ecl
+        else parse_args argv (idx + 1) (Some arg) ecl format
 
 let () =
-  let data, ecl = parse_args (Sys.get_argv ()) 1 None Config.ECL.M in
+  let data, ecl, format =
+    parse_args (Sys.get_argv ()) 1 None Config.ECL.M Ascii
+  in
   match data with
-  | Some d -> run d ecl
+  | Some d -> run d ecl format
   | None ->
       Out_channel.output_string Out_channel.stderr
         "Error: DATA argument is required\n";
